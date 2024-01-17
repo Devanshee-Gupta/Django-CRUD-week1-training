d15f066bf0fe20cd02d0e5e4f99dd2a7a663fc36
Devanshee Gupta <consultadd@Consultadds-MacBook-Pro.local>
2024-01-17 21:06:23 +0530

    some files changed

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from home.models import Employee,Departments
 # Register your models here.
+
+
--- home/models.py
+++ home/models.py
@@ -7,10 +7,8 @@ class Employee(models.Model):
     Employee_name = models.CharField(max_length = 50)
 
 
-
 class Departments(models.Model):
     Department_id = models.IntegerField(primary_key = True)
     Department_name = models.CharField(max_length=50)
     Department_manager = models.OneToOneField(Employee, related_name='manager', on_delete=models.CASCADE, null = True) 
-
 
